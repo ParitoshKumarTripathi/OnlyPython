c07f0e13a8f4f740e7bbfe92d2d5d8f4eb556e1f
Paritosh Kumar Tripathi <126463558+ParitoshKumarTripathi@users.noreply.github.com>
2023-08-26 18:05:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import feedparser
+from newsplease import NewsPlease
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def news():
+    feed = feedparser.parse('https://rss.nytimes.com/services/xml/rss/nyt/World.xml')
+    titles = []
+
+    for entry in feed.entries:
+        article = NewsPlease.from_url(entry.link)
+        if article is not None:
+            titles.append(article.title)
+
+    all_titles = '\n'.join(titles)
+    return all_titles
